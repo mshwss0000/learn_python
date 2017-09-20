c613576943ba2e7e179d7fc51ecdc938009fece4
mshwss0000 <464875600@qq.com>
2017-09-20 10:53:24 +0800

    2

---
+++ 111.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+while True:
+    m = input()
+    n = input()
+    #中间的我省略了
+    print(m)
+    x = input('请问继续运行吗?输入1停止运行，输入其他继续运行')
+    if x == "1":
+        break
